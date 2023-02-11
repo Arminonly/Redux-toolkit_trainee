aa30714e46b534b45821e46ba8b2d7e64e16f590
Arminonly <amiaby2016@gmail.com>
2023-02-11 17:16:18 +0300

    complitedTodo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
+
   const addTodo = () => {
     if (text.trim().length) {
       setTodos([
@@ -14,9 +15,24 @@ function App() {
           complited: false
         }
       ]);
-      setText('')
+      setText('');
     }
   };
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+  const complitedTodo = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          complited: !todo.complited
+        };
+      })
+    );
+  };
+
   return (
     <div className="App">
       <label>
@@ -24,15 +40,21 @@ function App() {
         <button onClick={addTodo}>Add todo</button>
       </label>
       <ul>
-        {
-        todos.map(todo=><li key={todo.id}>
-          <input type="checkbox"/>
-          <span>
-            {todo.text}
-          </span>
-          <span className='delete'>&times;</span>
-        </li>)
-        }
+        {todos.map(todo =>
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.complited}
+              onChange={() => complitedTodo(todo.id )}
+            />
+            <span>
+              {todo.text}
+            </span>
+            <span className="delete" onClick={() => removeTodo(todo.id)}>
+              &times;
+            </span>
+          </li>
+        )}
       </ul>
     </div>
   );
